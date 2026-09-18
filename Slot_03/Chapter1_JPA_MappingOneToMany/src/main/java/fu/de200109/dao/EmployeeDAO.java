aeccbd6569f541d64de7fa5a2efc95b0d688460b
duongtrong06tk <duongtrong06.tk@gmail.com>
2026-09-18 10:28:26 +0700

    feat(TODO2.5): save method for EmployeeDAO

--- Slot_03/Chapter1_JPA_MappingOneToMany/src/main/java/fu/de200109/dao/EmployeeDAO.java
+++ Slot_03/Chapter1_JPA_MappingOneToMany/src/main/java/fu/de200109/dao/EmployeeDAO.java
@@ -1,4 +1,27 @@
 package fu.de200109.dao;
 
+import fu.de200109.pojo.Employee;
+import fu.de200109.util.JPAUtil;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+import java.util.List;
+
 public class EmployeeDAO {
+
+    public void save(Employee employee) {
+        EntityManager em = JPAUtil.getEMF().createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        try {
+            tx.begin();
+            em.persist(employee);
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null && tx.isActive()) tx.rollback();
+            throw e;
+        } finally {
+            em.close();
+        }
+    }
+
+
 }
